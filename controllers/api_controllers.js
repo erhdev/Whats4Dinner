927602e983da373496005789a73689b1e0d795b6
BShieh <rebeccasshieh@gmail.com>
2019-06-11 17:29:14 -0400

    wip: changed routes to arrow functions, testing routes

--- controllers/api_controllers.js
+++ controllers/api_controllers.js
@@ -1,25 +1,24 @@
 const db = require("../models");
 const express = require("express");
-const exphbs = require("express-handlebars");
-// const router = express.Router();
+const router = express.Router();
 
-const app = express();
 
-app.engine("handlebars", exphbs());
-app.set("view engine", "handlebars");
+
+
 //Utilizes models folder to create our routes.
-module.exports = function(app) {
+
+
   //Retrieves list of all restaurants and includes the menu items associated with those restaurants.
-  app.get("/", (req, res) => {
+  router.get("/", (req, res) => {
     db.Restaurant.findAll({
-      include: models.Menu_Item
+      include: [db.Menu_Item]
     }).then(menu_item => {
-      res.render("index", { menu_item: menu_item });
+      res.render("index", menu_item);
       console.log(menu_item);
     });
   });
 
-  app.get("/restaurant/:id", (req, res) => {
+  router.get("/restaurant/:id", (req, res) => {
     db.Restaurant.findOne({
       where: {
         id: req.params.id
@@ -29,7 +28,7 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/food/:id", (req, res) => {
+  router.get("/food/:id", (req, res) => {
     db.Food.findOne({
       where: {
         id: req.params.id
@@ -38,4 +37,6 @@ module.exports = function(app) {
       res.json(dbRestaurant);
     });
   });
-};
+
+
+  module.exports = router;
