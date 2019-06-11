61ab391342e00fda7904a48d8c8e5957c95c83d8
Kevin Schroeder <schroederkevin94@gmail.com>
2019-06-11 07:31:41 -0400

    pulling develop

--- test/mocks/mocks.test.js
+++ test/mocks/mocks.test.js
@@ -4,7 +4,8 @@ const {
     checkModelName,
     checkPropertyExists
 } = require('sequelize-test-helpers')
-const Restaurant = require('../../models/restaurant')
+const Restaurant = require('../../models/restaurant');
+const Menu_Item = require('../../models/menu_item');
 
 describe('restaurant model', () => {
     const Model = Restaurant(sequelize, dataTypes)
@@ -13,4 +14,16 @@ describe('restaurant model', () => {
     context('properties', () => {
         ;['restaurant_name', 'restaurant_category'].forEach(checkPropertyExists(instance));
     });
+
+    // context('check associations', () => {
+    //     //const Menu_Item = 'some other model' // it doesn't matter what
+    //     before(() => {
+    //         Model.associate({Menu_Item})
+    //     })
+    //     it('defined a belongsTo association with Menu_Item', () => {
+    //         expect(Restaurant.hasMany).to.have.been.calledWith(Menu_Item);
+    //     });
+    // });
 });
+
+
