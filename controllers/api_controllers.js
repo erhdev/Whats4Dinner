aea700cd6bb984128627064ae41ccb5d3ef46bfd
Kevin Schroeder <schroederkevin94@gmail.com>
2019-06-11 21:13:33 -0400

    added menu_item route

--- controllers/api_controllers.js
+++ controllers/api_controllers.js
@@ -13,19 +13,19 @@ router.get("/", (req, res) => {
     db.Restaurant.findAll({
         include: [db.Menu_Item]
     }).then(function (restaurant) {
-        res.render('index', { restaurant: restaurant });
-        // res.json({ status: 200, restaurant: restaurant });
+        //res.render('index', { restaurant: restaurant });
+        res.json({ status: 200 });
+        //for (i = 0; i)
+        res.json({ restaurants: restaurant });
     });
 });
 
 router.get("/restaurant/:id", (req, res) => {
-    console.log(req.params);
     db.Restaurant.findOne({
         where: {
             id: req.params.id
         }
     }).then(result => {
-
         db.Menu_Item.findAll({
             where: {
                 RestaurantId: result.id
@@ -33,23 +33,22 @@ router.get("/restaurant/:id", (req, res) => {
         }).then(result => {
             res.json({ status: 200, message: result });
         });
-
-
-
-
-
-        // console.log(result);
-        // res.json({ status: 200, message: result });
     });
 });
 
 router.get("/menu_item/:id", (req, res) => {
-    db.Menu_Item.findOne({
+    db.Menu_Item.findAll({
         where: {
             id: req.params.id
         }
     }).then(result => {
-        res.json({ status: 200, message: result });
+        db.Recipe.findAll({
+            where: {
+                MenuItemId: result[0].id
+            }
+        }).then(result => {
+            res.json({ status: 200, message: result });
+        });
     });
 });
 
