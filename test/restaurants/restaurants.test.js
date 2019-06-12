aa5dada2c75abc7c5fcde754cd6f6edb495772ca
Kevin Schroeder <schroederkevin94@gmail.com>
2019-06-12 10:18:19 -0400

    WIP

--- test/restaurants/restaurants.test.js
+++ test/restaurants/restaurants.test.js
@@ -1,44 +1,108 @@
+process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const db = require('../../models');
 const express = require('express');
 const apiRouter = require('../../controllers/api_controllers');
 const should = chai.should();
+const server = require('../../server');
+// Setting up the chai http plugin
 chai.use(chaiHttp);
 
-describe("GET /", () => {
-    let app;
+let request;
 
-    beforeEach(() => {
-        app = express();
-        app.use('/', apiRouter);
+// describe("GET /", () => {
+//     let app;
+//     beforeEach(() => {
+//         app = express();
+//         app.use('/', apiRouter);
+//     });
+//     it("It should respond with an object and a 200 status", done => {
+//         chai.request(app)
+//             .get('/')
+//             .end((err, res) => {
+//                 res.should.have.status(200);
+//                 res.body.should.be.a('Object');
+//                 done();
+//             });
+//     });
+// });
+
+describe("GET /", function () {
+    // Before each test begins, create a new request server for testing
+    // & delete all examples from the db
+    beforeEach(function () {
+        request = chai.request(server);
+        return db.sequelize.sync({ force: true });
     });
 
-    it("It should respond with an object", done => {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('Object');
+    it("should find all restaurants", function (done) {
+        // Add some examples to the db to test with
+        console.log("HERE");
+        db.Restaurants.bulkCreate([
+            { restaurant_name: "First Restaurant", restaurant_category: "First Cateogry" },
+            { restaurant_name: "Second Restaurant", restaurant_category: "Second Cateogry" }
+        ]).then(function () {
+            // Request the route that returns all examples
+            request.get("/").end(function (err, res) {
+                var responseStatus = res.status;
+                var responseBody = res.body;
+
+                // Run assertions on the response
+
+                expect(err).to.be.null;
+
+                expect(responseStatus).to.equal(200);
+
+                expect(responseBody)
+                    .to.be.an("array")
+                    .that.has.lengthOf(2);
+
+                expect(responseBody[0])
+                    .to.be.an("object")
+                    .that.includes({ text: "First Restaurant", description: "First Cateogry" });
+
+                expect(responseBody[1])
+                    .to.be.an("object")
+                    .that.includes({ text: "Second Restaurant", description: "Second Cateogry" });
+
+                // The `done` function is used to end any asynchronous tests
                 done();
             });
+        });
     });
 });
 
-describe("GET /restaurant/:id", () => {
-    let app;
+// describe("GET /restaurant/:id", () => {
+//     let app;
+//     beforeEach(() => {
+//         app = express();
+//         app.use('/restaurant/:id', apiRouter);
+//     });
+//     it("It should respond with an object and a 200 status", done => {
+//         chai.request(app)
+//             .get('/restaurant/:id')
+//             .end((err, res) => {
+//                 res.should.have.status(200);
+//                 res.body.should.be.a('Object');
+//                 done();
+//             });
+//     });
+// });
 
-    beforeEach(() => {
-        app = express();
-        app.use('/restaurant/:id', apiRouter);
-    });
-
-    it("It should respond with an object", done => {
-        chai.request(app)
-            .get('/restaurant/:id')
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('Object');
-                done();
-            });
-    });
-});
+// describe("GET /category/:category", () => {
+//     let app;
+//     beforeEach(() => {
+//         app = express();
+//         app.use('/category/:category', apiRouter);
+//     });
+//     it("It should respond with an object and a 200 status", done => {
+//         chai.request(app)
+//             .get('/category/:category')
+//             .end((err, res) => {
+//                 res.should.have.status(200);
+//                 res.body.should.be.a('Object');
+//                 done();
+//             });
+//     });
+// });
