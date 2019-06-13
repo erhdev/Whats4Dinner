460e1048f8faa53e58489862eaa79adfa49653af
Kevin Schroeder <schroederkevin94@gmail.com>
2019-06-13 07:45:58 -0400

    Pulling from develop

--- test/restaurants/restaurants.test.js
+++ test/restaurants/restaurants.test.js
@@ -4,7 +4,7 @@ const chaiHttp = require('chai-http');
 const db = require('../../models');
 const express = require('express');
 const apiRouter = require('../../controllers/api_controllers');
-const should = chai.should();
+//const should = chai.should();
 const expect = chai.expect;
 const server = require('../../server');
 // Setting up the chai http plugin
@@ -26,15 +26,12 @@ describe("GET /", function () {
         db.Restaurant.bulkCreate([
             { restaurant_name: "First Restaurant", restaurant_category: "First Cateogry" },
             { restaurant_name: "Second Restaurant", restaurant_category: "Second Cateogry" }
-        ]).then(db.Menu_Item.bulkCreate([
-            { menu_item: "First item"},
-            { menu_item: "Second item"}
-        ])).then(function () {
+        ]).then(function () {
             // Request the route that returns all examples
             request.get("/").end(function (err, res) {
-                console.log(res);
+                //console.log(res);
                 var responseStatus = res.status;
-                var responseBody = res.body.restaurants;
+                var responseBody = res.body;
 
                 // Run assertions on the response
 
@@ -61,6 +58,51 @@ describe("GET /", function () {
     });
 });
 
+describe("GET /restaurant/:id", function () {
+    // Before each test begins, create a new request server for testing
+    // & delete all examples from the db
+    beforeEach(function () {
+        request = chai.request(server);
+        return db.sequelize.sync({ force: true });
+    });
+
+    it("should find all restaurants", function (done) {
+        // Add some examples to the db to test with
+        console.log("HERE");
+        db.Restaurant.bulkCreate([
+            { restaurant_name: "First Restaurant", restaurant_category: "First Cateogry" },
+            { restaurant_name: "Second Restaurant", restaurant_category: "Second Cateogry" }
+        ]).then(function () {
+            // Request the route that returns all examples
+            request.get("/restaurant/1").end(function (err, res) {
+                console.log(res);
+                var responseStatus = res.status;
+                var responseBody = res.body;
+
+                // Run assertions on the response
+
+                expect(err).to.be.null;
+
+                expect(responseStatus).to.equal(200);
+
+                expect(responseBody)
+                    .to.be.an("array")
+                    .that.has.lengthOf(1);
+
+                expect(responseBody[0])
+                    .to.be.an("object")
+                    .that.includes({ restaurant_name: "First Restaurant", restaurant_category: "First Cateogry" });
+
+                expect(responseBody[1])
+                    .to.be.an("object")
+                    .that.includes({ restaurant_name: "Second Restaurant", restaurant_category: "Second Cateogry" });
+
+                // The `done` function is used to end any asynchronous tests
+                done();
+            });
+        });
+    });
+});
 // describe("GET /", () => {
 //     let app;
 //     beforeEach(() => {
