46c6f7131b7cc46e7aeac21828e867610994e909
BShieh <rebeccasshieh@gmail.com>
2019-06-14 12:36:03 -0400

    wip: updating api info to food2fork

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -1,27 +1,26 @@
 //Global variables
 const axios = require("axios");
-let ingredient = "";
-const apiKey = "c13f6b9c29mshacae678a7d2f47cp14ffecjsn26928bb60cb5";
-const url =
-  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=" +
-  ingredient;
+let ingredient = "chicken";
+const apiKey = "17451c0032296d12482577496344c284";
+const url = "https://www.food2fork.com/api/get?key=" + apiKey + "q=" + ingredient;
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    "X-RapidAPI-Host": "community-food2fork.p.rapidapi.com",
     "X-RapidAPI-Key": apiKey
   }
 };
 
 $(function() {
-    $(".api-entry").on("click", function(event) {
-      var ingredient = $(this).data("api-entry");
-    });
-    
-
-axios
-  .get(url, options)
-  .then(response => {
-    console.log(response);
-  })
-  .catch(error => console.log(error));
+  $("#searchSubmit").on("click", function(event) {
+    let ingredient = $(this).data("api-entry");
+    axios
+      .get(url, options)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+});
