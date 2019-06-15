ad688d1f18cfd80a2923fed41f322304e69ae1ab
Kevin Schroeder <schroederkevin94@gmail.com>
2019-06-15 12:17:22 -0400

    Linting WIP

--- config/config.js
+++ config/config.js
@@ -1,34 +1,34 @@
 // This file connects to the database
-var mysql = require("mysql");
+// const mysql = require('mysql');
 require('dotenv').config();
-const fs = require('fs');
+// const fs = require('fs');
 
 module.exports = {
-    development: {
-        insecureAuth: true,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: "Whats4Dinner_db",
-        host: process.env.DB_HOST,
-        dialect: "mysql",
-        logging: false
-        
-    },
-    test: {
-        username: process.env.TEST_DB_USER,
-        password: process.env.TEST_DB_PASS,
-        database: "test_Whats4Dinner_db",
-        host: process.env.TEST_DB_HOST,
-        dialect: "mysql",
-        logging: false,
-        //insecureAuth: true
-    },
-    production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: "Whats4Dinner_db",
-        host: process.env.DB_HOST,
-        dialect: "mysql",
-        insecureAuth: true
-    }
+  development: {
+    insecureAuth: true,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'Whats4Dinner_db',
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+
+  },
+  test: {
+    username: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PASS,
+    database: 'test_Whats4Dinner_db',
+    host: process.env.TEST_DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+    // insecureAuth: true
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'Whats4Dinner_db',
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    insecureAuth: true,
+  },
 };
